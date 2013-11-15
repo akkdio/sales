63eeedd1155c3616c5fa820bc80c3ad83596ff91
akkdio <akkdio@gmail.com>
2013-11-15 17:34:19 -0500

    forgot the migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112234552) do
+ActiveRecord::Schema.define(version: 20131115202509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20131112234552) do
   end
 
   add_index "sales", ["product_id"], name: "index_sales_on_product_id", using: :btree
+
+  create_table "stripe_events", force: true do |t|
+    t.string   "stripe_id"
+    t.string   "stripe_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
